1e5c3df47f1785f9c8e74cdd7d8634f2de35f218
Aidan <100976971+Borngle@users.noreply.github.com>
2025-10-24 00:58:43 +0100

    Initialising project

---
+++ src/main/java/io/github/borngle/rhyme/core/Main.java
@@ -0,0 +1,21 @@
+/*********************************************************************
+ * @file        Main.java
+ * @author      Aidan
+ * @date        24-10-2025
+ * @brief       Entry point into the program
+ *
+ * @details     Sequentially executes the steps required to transcribe a MIDI file to tablature format
+ *              and outputs the resulting tablature
+ *
+ * @note        None
+ *
+ * @references  None
+ *********************************************************************/
+
+package io.github.borngle.rhyme.core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
